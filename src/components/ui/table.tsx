bc19e28765f639b745ed1323def348fd0266fac7
narendra <radioactivenarendra@gmail.com>
2025-11-16 12:00:45 +0530

    ui fix

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -12,8 +12,7 @@ function Table({ className, ...props }: React.ComponentProps<'table'>) {
     >
       <table
         data-slot='table'
-        className={cn('w-full caption-bottom text-sm', className)}
-        style={{ minWidth: 'max-content' }}
+        className={cn('w-full caption-bottom text-sm min-w-0', className)}
         {...props}
       />
     </div>
